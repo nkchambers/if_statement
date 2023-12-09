0494466e0b613a1b5be73d09e22024b151a3cc6a
nkchambers <nkchames@gmail.com>
2023-12-08 16:48:38 -0800

    adding if_statement for 12-8-23

---
+++ if_statement.js
@@ -0,0 +1,17 @@
+// CODECADEMY JAVASCRIPT PRACTICE - if_statement assignment
+
+// Task 1 - Using the let keyword, declare a variable named sale. Assign the value true to it.
+let sale = true;
+
+// Task 3 part - reaasign sale var to false
+// sale = false;
+
+// Task 2 - Now create an if statement. Provide the if statement a condition of sale.
+// Inside the code block of the if statement, console.log() the string 'Time to buy!'.
+if (sale === true) {
+    console.log('Time to buy!');
+}
+
+// Task 3 - Notice that the code inside the if statement ran, since 'Time to buy!' was logged to the console.
+// Below the sale variable declaration, but before the if statement, reassign sale to false. 
+// Run your code and observe what happens, we’ll be changing this behavior in the next exercise.
